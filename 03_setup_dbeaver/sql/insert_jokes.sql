fc2905441b5662ad39ef1d0a3fd25c028fac2f0f
Katrin katryl-yh <katrin@rylander.eu>
2024-11-12 13:01:08 +0100

    setting up dbeaver

---
+++ 03_setup_dbeaver/sql/insert_jokes.sql
@@ -0,0 +1,23 @@
+INSERT
+	INTO
+	funny_jokes (id,
+	joke_text,
+	rating
+)
+	
+VALUES
+(0, 
+'supah fun joke',
+1),
+(1,
+'Why don’t scientists trust atoms? Because they make up everything!' ,
+8),
+(2,
+'Why did the scarecrow win an award? Because he was outstanding in his field!',
+7),
+(3,
+'I told my wife she was drawing her eyebrows too high. She looked surprised.',
+9),
+(4,
+'Why don’t skeletons fight each other? They don’t have the guts.',
+6);
---
+++ 03_setup_dbeaver/sql/queries.sql
@@ -0,0 +1,3 @@
+ SELECT * FROM funny_jokes;
+ 
+
